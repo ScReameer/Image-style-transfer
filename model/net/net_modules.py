591b080cbdb47d7df6e22238addd6eea57e1e048
Vadim <vingvadim@gmail.com>
2024-03-12 21:24:19 +0300

    added inference examples

--- model/net/net_modules.py
+++ model/net/net_modules.py
@@ -2,32 +2,34 @@ import keras
 import tensorflow as tf
 
 class ReflectionPadding2D(keras.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-    def call(self, x):
+    def call(self, x) -> tf.Tensor:
         return tf.pad(x, tf.constant([[0,0], [1,1], [1,1], [0,0]]), mode="REFLECT")
 
-def compute_mean_std(x:tf.Tensor, epsilon=1e-5):
+def compute_mean_std(x:tf.Tensor, epsilon=1e-5) -> tuple:
     axes = [1, 2]
     mean, variance = tf.nn.moments(x, axes=axes, keepdims=True)
     standard_deviation = tf.sqrt(variance + epsilon)
     return mean, standard_deviation
 
 class AdaIN(keras.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.trainable = False
         self.build((None, None, 512))
 
-    def call(self, content:tf.Tensor, style=tf.Tensor, alpha=1):
+    def call(self, content:tf.Tensor, style=tf.Tensor, alpha=1) -> tf.Tensor:
         content_mean, content_std = compute_mean_std(content)
         style_mean, style_std = compute_mean_std(style)
         t = style_std * ((content - content_mean) / content_std) + style_mean
+        # Interpolate between content and style using alpha:
+        # less alpha -> less style
         return alpha * t + ((1 - alpha) * content)
 
 class Encoder(keras.Model):
-    def __init__(self, input_shape=(None, None, 3)):
+    def __init__(self, input_shape=(None, None, 3)) -> None:
         super().__init__()
         self.input_shape = input_shape
         vgg19 = keras.applications.VGG19(
@@ -42,7 +44,9 @@ class Encoder(keras.Model):
         self.encoder = keras.Sequential()
         for layer in mini_vgg19.layers:
             if isinstance(layer, keras.layers.Conv2D):
+                # Weights of vgg19 conv layer
                 W_init = keras.initializers.Constant(layer.get_weights()[0])
+                # Bias of vgg19 conv layer
                 B_init = keras.initializers.Constant(layer.get_weights()[1])
                 self.encoder.add(ReflectionPadding2D())
                 self.encoder.add(
@@ -63,7 +67,7 @@ class Encoder(keras.Model):
         self.encoder.trainable = False
         self.build(self.input_shape)
 
-    def call(self, inputs, return_only_last=False):
+    def call(self, inputs, return_only_last=False) -> tf.Tensor:
         # relu4_1
         block4 = keras.Model(
             inputs=self.encoder.inputs,
@@ -71,6 +75,7 @@ class Encoder(keras.Model):
         )
         # Last featuremap of encoder
         out4 = block4(inputs)
+        # Inference speed optimization
         if return_only_last:
             return out4
         # relu1_1
@@ -95,7 +100,7 @@ class Encoder(keras.Model):
         return out1, out2, out3, out4
     
 class Decoder(keras.Model):
-    def __init__(self, input_shape=(None, None, 512)):
+    def __init__(self, input_shape=(None, None, 512)) -> None:
         super().__init__()
         self.input_shape = input_shape
         self.conv_config = dict(
@@ -103,6 +108,7 @@ class Decoder(keras.Model):
             activation='relu',
             padding='valid'
         )
+        # Mirrored encoder
         self.decoder = keras.Sequential([
             keras.layers.InputLayer(self.input_shape),
             ReflectionPadding2D(),
@@ -129,6 +135,6 @@ class Decoder(keras.Model):
         ], name='decoder')
         self.build(self.input_shape)
 
-    def call(self, inputs):
+    def call(self, inputs) -> tf.Tensor:
         return self.decoder(inputs)
     
--- model/net/style_autoencoder.py
+++ model/net/style_autoencoder.py
@@ -5,8 +5,7 @@ from skimage.exposure import match_histograms
 from net.net_modules import Decoder, Encoder, AdaIN, compute_mean_std
 
 class StyleTransfer(keras.Model):
-    
-    def __init__(self, style_weight=10, content_weight=1, **kwargs):
+    def __init__(self, style_weight=20, content_weight=1, **kwargs) -> None:
         super().__init__(**kwargs)
         self.input_shape = (None, None, 3)
         self.adain = AdaIN()
@@ -22,31 +21,31 @@ class StyleTransfer(keras.Model):
         self.imgnet_mean_bgr = tf.constant([103.939, 116.779, 123.68], dtype=tf.float32)
         self.build(self.input_shape)
 
-    def get_build_config(self):
+    def get_build_config(self) -> dict:
         build_config = super().get_build_config()
         return build_config
 
-    def build_from_config(self, config):
+    def build_from_config(self, config) -> None:
         self.build(config["input_shape"])
         
-    def compile(self, optimizer, loss_fn, **kwargs):
+    def compile(self, optimizer, loss_fn, **kwargs) -> None:
         super().compile(**kwargs)
         self.optimizer = optimizer
         self.loss_fn = loss_fn
         
-    def get_compile_config(self):
+    def get_compile_config(self) -> dict:
         return {
             "optimizer": self.optimizer,
             "loss_fn": self.loss_fn,
         }
         
-    def compile_from_config(self, config):
+    def compile_from_config(self, config) -> None:
         optimizer = keras.utils.deserialize_keras_object(config["optimizer"])
         loss_fn = keras.utils.deserialize_keras_object(config["loss_fn"])
         self.compile(optimizer=optimizer, loss_fn=loss_fn)
 
-    def preprocess_input(self, content_img:tf.Tensor, style_img:tf.Tensor, resize=None):
-        # Unscaled decentered RGB (uint8) -> scaled centered BGR (float32)
+    def preprocess_input(self, content_img:tf.Tensor, style_img:tf.Tensor, resize=None) -> tuple:
+        # Decentered RGB (uint8) -> centered BGR (float32)
         content_img = tf.cast(content_img[..., ::-1], tf.float32) - self.imgnet_mean_bgr
         style_img = tf.cast(style_img[..., ::-1], tf.float32) - self.imgnet_mean_bgr
         # Resize for inference
@@ -55,14 +54,14 @@ class StyleTransfer(keras.Model):
             style_img = self.resizer(*resize)(style_img)
         return content_img, style_img
 
-    def postprocess_output(self, reconstructed_image:tf.Tensor, output_shape):
-        # Scaled centered BGR (float32) -> unscaled decentered RGB (uint8)
+    def postprocess_output(self, reconstructed_image:tf.Tensor, output_shape) -> tf.Tensor:
+        # Centered BGR (float32) -> decentered RGB (uint8)
         denormalized_img = tf.cast(tf.clip_by_value((reconstructed_image + self.imgnet_mean_bgr), 0, 255), tf.uint8)
         # Resize to original shape of content image
         resized_img = self.resizer(*output_shape)(denormalized_img)
         return resized_img[..., ::-1]
 
-    def train_step(self, inputs):
+    def train_step(self, inputs) -> dict:
         content, style = self.preprocess_input(*inputs)
         loss_content = 0.0
         loss_style = 0.0
@@ -107,7 +106,7 @@ class StyleTransfer(keras.Model):
         }
         
     # Inference
-    def call(self, content:tf.Tensor, style:tf.Tensor, alpha=1, save_content_colors=False):
+    def call(self, content:tf.Tensor, style:tf.Tensor, alpha=1, save_content_colors=False) -> tf.Tensor:
         output_shape = content.shape[1:3]
         # Transfer colors from content to output via histogram matching
         if save_content_colors:
